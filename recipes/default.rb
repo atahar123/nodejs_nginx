3389f05551abff6b85cf05208a4e21c691c677c5
Atahar Uddin <AtaharUddin@tcadmins-air-5.home>
2020-04-07 11:56:53 +0100

    tests ran successfully

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,16 +1,39 @@
-#
 # Cookbook:: nodejs_nginx
 # Recipe:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
+apt_update 'update_sources' do
+  action :update
+end
 
 package 'nginx'
 package 'nodejs'
+
+package %w(nginx npm)
+
+npm_package 'pm2'
+npm_package 'react'
 
 service 'nginx' do
   action [:enable, :start]
 end
 
-template '/etc/nginx/sites-available/proxy.conf' do
+template '/etc/nginx/sites-available/proxy.conf.erb' do
   source 'proxy.conf.erb'
+  notifies :restart, 'service[nginx]'
 end
+
+link '/etc/nginx/sites-enabled/proxy.conf.erb' do
+  to '/etc/nginx/sites-available/proxy.conf.erb'
+  notifies :restart, 'service[nginx]'
+end
+
+link '/etc/nginx/sites-enabled/default' do
+  action :delete
+  notifies :restart, 'service[nginx]'
+end
+
+include_recipe 'nodejs'
+
+nodejs_npm 'pm2'
+nodejs_npm 'react'
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -13,3 +13,20 @@ describe service('nginx') do
   it { should be_running }
   it { should be_enabled }
 end
+
+describe http('http://localhost') do
+  its('status') { should cmp 502 }
+end
+
+describe package('nodejs') do
+  it { should be_installed }
+  its('version') { should cmp > '8.10.0'}
+end
+
+describe npm('pm2') do
+  it { should be_installed }
+end
+
+describe npm('react') do
+  it { should be_installed }
+end
